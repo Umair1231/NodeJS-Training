aa061c7c16e5a6afc5de4878046a475cdf365a0f
Umair1231 <umairgoku321@gmail.com>
2023-12-27 16:43:36 +0500

    testing deployment

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -9,6 +9,7 @@ const passport = require('passport')
 const cookieParser = require('cookie-parser')
 const serverless = require("serverless-http");
 require("dotenv").config();
+const router = express.router();
 
 
 //create express app
@@ -35,27 +36,34 @@ app.use(passport.initialize())
 app.use(cookieParser())
 
 
-fs.readdir(`../routes`, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
+router.get('/', (req,res) => {
+  res.json( { message: "Test" } )
+})
 
-  files.forEach(file => {
-    if (file.endsWith('Route.js')) {
-      let routeName = '/' + file.substring(0, file.indexOf('Route')).toLowerCase();
-      const completeName = file.replace('.js', '');
-      const routeFunction = require(`../routes/${completeName}`);
+app.use(`/.netlify/functions/api`, router);
 
-      if (typeof routeFunction === 'function') {
-        routeName = '/.netlify/functions/api' + routeName;
-        app.use(routeName, routeFunction);
-      } else {
-        console.error(`Route file '${completeName}' does not export a valid function.`);
-      }
-    }
-  });
-});
+
+// fs.readdir(`../routes`, (err, files) => {
+//   if (err) {
+//     console.error('Error reading directory:', err);
+//     return;
+//   }
+
+//   files.forEach(file => {
+//     if (file.endsWith('Route.js')) {
+//       let routeName = '/' + file.substring(0, file.indexOf('Route')).toLowerCase();
+//       const completeName = file.replace('.js', '');
+//       const routeFunction = require(`../routes/${completeName}`);
+
+//       if (typeof routeFunction === 'function') {
+//         routeName = '/.netlify/functions/api' + routeName;
+//         app.use(routeName, routeFunction);
+//       } else {
+//         console.error(`Route file '${completeName}' does not export a valid function.`);
+//       }
+//     }
+//   });
+// });
 
 
 //listening for requests on port 3000
